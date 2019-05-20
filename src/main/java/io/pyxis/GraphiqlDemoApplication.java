bb0a3ca342a787cafd1e5a7bda2c53710eab0460
Ihor Didyk <ihordidyk95@gmail.com>
2019-05-20 12:31:30 +0300

    created project with basic configuration

---
+++ src/main/java/io/pyxis/GraphiqlDemoApplication.java
@@ -0,0 +1,13 @@
+package io.pyxis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphiqlDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphiqlDemoApplication.class, args);
+    }
+
+}
