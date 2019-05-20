87019c8b37347f6ee753e98b45d65ec228c2aa69
Ihor Didyk <ihordidyk95@gmail.com>
2019-05-20 13:08:26 +0300

    implemented domail layer

---
+++ src/main/java/io/pyxis/domain/model/Comment.java
@@ -0,0 +1,30 @@
+package io.pyxis.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "COMMENTS")
+@Data
+public class Comment  extends BaseEntity{
+
+    @Column(nullable = false)
+    @Lob
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "post_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
+    @JsonProperty("post_id")
+    private Post post;
+
+}
---
+++ src/main/java/io/pyxis/domain/model/Post.java
@@ -0,0 +1,27 @@
+package io.pyxis.domain.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "POSTS")
+@Data
+public class Post  extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 100)
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Size(max = 250)
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    @Lob
+    private String content;
+}
